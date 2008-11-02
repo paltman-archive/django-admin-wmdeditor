35ce1eca065ce3460207ec06ea7f7ddf206a6bf5
Stephan Jaekel <steph@rdev.info>
2008-11-02 15:41:13 +0100

    added modelform and markdown widget

---
+++ admin_wmdeditor/widgets.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.utils.safestring import mark_safe
+from django.conf import settings
+
+class WmdEditorWidget(forms.Textarea):
+    def render(self, name, value, attrs=None):
+        output = [super(WmdEditorWidget, self).render(name, value, attrs)]
+        output.append(u'<label>&nbsp;</label><div id="id_%s_preview" class="wmd-preview"></div>' % name.lower())
+        output.append(u'<script type="text/javascript">create_wmdeditor("%s");</script>' % name.lower())
+        return mark_safe(u''.join(output))
+
+    class Media:
+        js = (
+            getattr(settings, 'WMDEDITOR_MEDIA_PREFIX', settings.MEDIA_URL + '/admin-wmdeditor') + '/starter.js',
+            getattr(settings, 'WMDEDITOR_MEDIA_PREFIX', settings.MEDIA_URL + '/admin-wmdeditor') + '/wmd/wmd.js',
+        )
+        css = {
+            'screen': (getattr(settings, 'WMDEDITOR_MEDIA_PREFIX', settings.MEDIA_URL + '/admin-wmdeditor') +'/preview.css',)
+        }
